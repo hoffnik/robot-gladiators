ec1180fb85b3862411bbb83061f35c673fda6f3e
hoffnik <hoffnik.dev@gmail.com>
2022-03-28 18:27:32 -0700

    added first variable and console.log to game

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,17 @@
-window.alert("This is an alert! JavaScript is running!");
+// This is a prompt
+var playerName = window.prompt ("What is your robot's name?");
+// Note the lack of quotation marks around playerName
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10+10);
+// what is this
+console.log("Our Robot's name is " + playerName)
+
+// This creates a function called "fight"
+function fight() {
+    window.alert("Thye fight has begun!");
+}
+
+//fight()
